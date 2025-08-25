7a01afcfcd3da0f4988218c795392ec72bad0f95
Vega22s <kusch4960@gmail.com>
2025-08-25 11:28:02 +0700

    fixUI and Exit

--- bin/connection_demo.dart
+++ bin/connection_demo.dart
@@ -1,4 +1,3 @@
-import 'package:connection_demo/connection_demo.dart' as connection_demo;
 import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -6,7 +5,7 @@ import 'dart:convert';
 void main() async {
   final user = await loginUser();
   if (user != null) {
-    await showMenu(user['id']);
+    await showMenu(user['id'], user['username']);
   }
 }
 
@@ -33,45 +32,24 @@ Future<Map<String, dynamic>?> loginUser() async {
   }
 }
 
-Future<void> showMenu(int userId) async {
+Future<void> showMenu(int userId, String username) async {
   while (true) {
-    print("\n======== Expense Tracking App ========");
-    print("1. Show all");
+    print("\n=========== Expense Tracking App ===========");
+    print("Welcome $username");
+    print("1. All expenses");
     print("2. Today's expense");
-    print("3. Exit");
-    stdout.write("Choose..");
+    print("3. Search expense");
+    print("4. Add new expense");
+    print("5. Delete an expense");
+    print("6. Exit");
+    stdout.write("Choose... ");
     final choice = stdin.readLineSync();
     final url = Uri.parse('http://localhost:3000/expenses?user_id=$userId');
     final response = await http.get(url);
     final expenses = json.decode(response.body) as List;
     final today = DateTime.now();
 
-    if (choice == '1') {
-      print("----------- All expenses -----------");
-      double total = 0;
-      int count = 1;
-      for (var e in expenses) {
-        print("$count. ${e['item']} : ${e['paid']}₮ : ${e['date']}");
-        total += e['paid'];
-        count++;
-      }
-      print("Total expenses = ${total.toStringAsFixed(0)}₮");
-    } else if (choice == '2') {
-      print("----------- Today's expenses -----------");
-      double total = 0;
-      int count = 1;
-      for (var e in expenses) {
-        final date = DateTime.parse(e['date']);
-        if (date.year == today.year &&
-            date.month == today.month &&
-            date.day == today.day) {
-          print("$count. ${e['item']} : ${e['paid']}₮ : ${e['date']}");
-          total += e['paid'];
-          count++;
-        }
-      }
-      print("Total expenses = ${total.toStringAsFixed(0)}₮");
-    } else if (choice == '3') {
+    if (choice == '6') {
       print("----- Bye ------");
       break;
     } else {
